7d643becf4237e42bac1795a368eef4ed26ef8fb
Sudin Bhattacharya <sudin.bhattacharya@gmail.com>
2014-05-11 12:07:47 -0400

    Moified plots 3 and 4 legends and corresponding code

--- plot3.R
+++ plot3.R
@@ -18,6 +18,6 @@ with(myDataSub, plot(DateTime, Sub_metering_1,
 with(myDataSub, points(DateTime, Sub_metering_1, type = "l", col = "black"))
 with(myDataSub, points(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(myDataSub, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", pch = "-", col = c("black", "red", "blue"), 
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
